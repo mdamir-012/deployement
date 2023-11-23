82e412a2cda9e2cf090aeadeeb16417a4021217c
unknown <mdamir4298@gmail.com>
2023-11-23 15:13:28 +0530

    env remove

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req,res)=>{
     res.send({message:"Api is working"})
 })
 app.get('/test', (req,res)=>{
-    res.send({message:"Api is working"+process.env.secret})
+    res.send({message:"Api is working and secret code is -" +process.env.secret})
 })
 
 app.listen(8000, ()=>{
